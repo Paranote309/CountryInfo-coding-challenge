a7bdcddfecb6a93dc65b20bb24fb952ba4f253f6
Paranote309 <78971278+Paranote309@users.noreply.github.com>
2023-04-25 23:41:00 +0100

    country info displays/creation wireframe
    -finish most of the functionality
    - starting the wireframe for bit of design of the web page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import React, { useState } from "react";
 
 function App() {
   const [country, setCountry] = useState("");
+  const [countryData, setCountryData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
+    setCountryData(null);
+    setError(null);
     try {
       const response = await fetch(
         `http://localhost:5000/api/country/${country}`
       );
       const data = await response.json();
       console.log(data); // TODO: Render country information to user
+      setIsLoading(false);
+      setCountryData(data);
     } catch (error) {
       console.error(error);
       // TODO: Render error message to user
+      setIsLoading(false);
+      setError("Unable to fetch country information");
     }
   };
 
@@ -31,6 +41,27 @@ function App() {
         </label>
         <button type="submit">Search</button>
       </form>
+
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {countryData && (
+        <div>
+          {/* <h2>{countryData.name}</h2>
+          <img src={countryData.flag} alt={`Flag of ${countryData.name}`} />
+          <p>Capital: {countryData.capital}</p>
+          <p>Population: {countryData.population}</p>*/}
+
+          {Object.entries(countryData).map(([key, value]) => (
+            <div key={key}>
+              {/* <p>{JSON.stringify(value)}</p> */}
+              {key === "name" && <p>Name: {value.common}</p>}
+              {key === "capital" && <p>Capital: {value[0]}</p>}
+              {key === "region" && <p>Region: {value}</p>}
+              {key === "population" && <p>Population: {value}</p>}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
